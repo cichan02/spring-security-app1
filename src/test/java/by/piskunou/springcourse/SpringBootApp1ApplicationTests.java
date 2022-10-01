9b9d521f5f30b80600cc58f7688d0f05fc89f4b0
cichan <6684177pt@gmail.com>
2022-10-02 01:33:53 +0300

    init

---
+++ src/test/java/by/piskunou/springcourse/SpringBootApp1ApplicationTests.java
@@ -0,0 +1,10 @@
+package by.piskunou.springcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApp1ApplicationTests {
+	@Test
+	void contextLoads() {}
+}

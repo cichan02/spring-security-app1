ab52f7e88820dcb2eacb23038bf6242ef132ea38
cichan <6684177pt@gmail.com>
2022-10-15 16:30:59 +0300

    add custom login page

--- src/main/java/by/piskunou/springcourse/config/SecurityConfig.java
+++ src/main/java/by/piskunou/springcourse/config/SecurityConfig.java
@@ -1,24 +1,45 @@
 package by.piskunou.springcourse.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import by.piskunou.springcourse.security.AuthProviderImpl;
+import by.piskunou.springcourse.services.PersonDetailsService;
 
 @SuppressWarnings("deprecation")
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	private final AuthProviderImpl authProvider;
+	private final PersonDetailsService personDetailsService;
 	
 	@Autowired
-	public SecurityConfig(AuthProviderImpl authProvider) {
-		this.authProvider = authProvider;
+	public SecurityConfig(PersonDetailsService personDetailsService) {
+		this.personDetailsService = personDetailsService;
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+			.authorizeRequests().antMatchers("/auth/login", "/error").permitAll()
+								.anyRequest().authenticated()
+			.and()
+			.formLogin().loginPage("/auth/login")
+						.loginProcessingUrl("/process_login")
+						.defaultSuccessUrl("/hello", true)
+						.failureUrl("/auth/login?error");
 	}
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) {
-		auth.authenticationProvider(authProvider);
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(personDetailsService);
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 }
